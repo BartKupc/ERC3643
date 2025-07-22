ed61a2efec1153f916e11fc563179f69dfa67a9c
bartkupc <kupcbart@gmail.com>
2025-07-22 01:07:55 +0100

    17

--- trex-scaffold/packages/react-app/src/components/phases/DeploymentPhase.js
+++ trex-scaffold/packages/react-app/src/components/phases/DeploymentPhase.js
@@ -215,7 +215,7 @@ const DeploymentPhase = () => {
 
   const getSigner = async () => {
     const privateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
-    const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545');
+          const provider = new ethers.providers.JsonRpcProvider('http://localhost:3001/api/hardhat');
     return new ethers.Wallet(privateKey, provider);
   };
 
