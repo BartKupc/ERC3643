b32609a3e384041ba0f227c32145cfdcc794368a
bartkupc <kupcbart@gmail.com>
2025-07-25 10:37:17 +0100

    last

--- oldcopy/frontend/phases/easy-deploy/constants.js
+++
@@ -1,9 +0,0 @@
-// Claim topic labels mapping
-export const CLAIM_TOPIC_LABELS = {
-  1: 'KYC (Know Your Customer)',
-  2: 'AML (Anti-Money Laundering)',
-  3: 'Accredited Investor',
-  4: 'EU Nationality Confirmed',
-  5: 'US Nationality Confirmed',
-  6: 'Blacklist'
-}; 
